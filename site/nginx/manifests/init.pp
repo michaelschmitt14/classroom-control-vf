a2ec91c66d0f8dfb61117ba34ae7cf3906ab03f5
michaelschmitt14 <michael.schmitt.14@gmail.com>
2016-08-03 13:56:18 -0500

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,46 @@
+class nginx {
+  package {'nginx':
+    ensure => present,
+  }
+  file {'/var/www':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+  }
+  file {'/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    sourece => 'puppet:///modules/nginx/index.html',
+  }
+  file {'/etc/nginx/nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    sourece => 'puppet:///modules/nginx/nginx.conf
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  file {'/etc/nginx/config.d':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+  }
+  file {'/etc/nginx/config.d/default.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    sourece => 'puppet:///modules/nginx/default.conf
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  service {'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
